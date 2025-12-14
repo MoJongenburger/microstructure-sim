169356e13a47fc48c62feb8d17de76531686584d
MoJongenburger <126263555+MoJongenburger@users.noreply.github.com>
2025-12-14 13:04:36 +0100

    Update trade.hpp
    add Trade

--- include/msim/trade.hpp
+++ include/msim/trade.hpp
@@ -1,1 +1,22 @@
+#pragma once
+#include "msim/types.hpp"
+
+namespace msim {
+
+struct Trade {
+  TradeId  id{};
+  Ts       ts{};
+
+  Price    price{};
+  Qty      qty{};
+
+  OrderId  maker_order_id{};
+  OrderId  taker_order_id{};
+};
+
+inline constexpr bool is_valid_trade(const Trade& t) noexcept {
+  return (t.qty > 0) && (t.price >= 0);
+}
+
+} // namespace msim
 
